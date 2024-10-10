d956f5b9d1f279f96d82db0220bf445f757cdf81
Fraser Hutchison <fraser@astria.org>
2024-10-10 13:24:45 +0100

    remove base64 encoding from telemetry

--- crates/astria-sequencer/src/mempool/transactions_container.rs
+++ crates/astria-sequencer/src/mempool/transactions_container.rs
@@ -116,8 +116,8 @@ impl fmt::Display for TimemarkedTransaction {
         write!(
             f,
             "tx_hash: {}, address: {}, signer: {}, nonce: {}, chain ID: {}",
-            telemetry::display::base64(&self.tx_hash),
-            telemetry::display::base64(&self.address),
+            core_utils::base64::display(&self.tx_hash),
+            core_utils::base64::display(&self.address),
             self.signed_tx.verification_key(),
             self.signed_tx.nonce(),
             self.signed_tx.chain_id(),
@@ -583,7 +583,7 @@ pub(super) trait TransactionsContainer<T: TransactionsForAccount> {
                 Ok(res) => res,
                 Err(error) => {
                     error!(
-                        address = %telemetry::display::base64(address),
+                        address = %core_utils::base64::display(address),
                         "failed to calculate new transaction cost when cleaning accounts: {error:#}"
                     );
                     continue;
@@ -795,7 +795,7 @@ impl PendingTransactions {
                     Err(error) => {
                         // mempool could be off due to node connectivity issues
                         error!(
-                            tx_hash = %telemetry::display::base64(&ttx.tx_hash),
+                            tx_hash = %core_utils::base64::display(&ttx.tx_hash),
                             "failed to add pending tx to builder queue: {error:#}"
                         );
                         continue;
